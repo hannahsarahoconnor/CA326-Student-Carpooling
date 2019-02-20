e448356138f6338c7b73214a71fb8f7c7008da06
catherine mooney <catherine.mooney33@mail.dcu.ie>
2019-02-20 17:14:17 +0000

    passenger profile

---
+++ code/Student_Carpooling/app/src/main/java/com/example/student_carpooling/ChatActivity.java
@@ -0,0 +1,4 @@
+package com.example.student_carpooling;
+
+public class ChatActivity {
+}
--- code/Student_Carpooling/app/src/main/java/com/example/student_carpooling/PassengerProfile.java
+++ code/Student_Carpooling/app/src/main/java/com/example/student_carpooling/PassengerProfile.java
@@ -1,7 +1,10 @@
 package com.example.student_carpooling;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,11 +17,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +32,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PassengerProfile extends AppCompatActivity
@@ -44,6 +56,17 @@ public class PassengerProfile extends AppCompatActivity
     private String email,UserID;
     private DatabaseReference UserDb;
     private String ProfilePicUrl;
+    private TextView Name,Username,Uni;
+
+    private ImageView profilePic;
+    private String DBName, DBUsername, DBUni;
+
+
+    private Uri ResultUri;
+    private Button Confirm;
+
+
+
 
 
     @Override
@@ -72,7 +95,29 @@ public class PassengerProfile extends AppCompatActivity
         Nemail = hView.findViewById(R.id.emailNav);
         navProfile = hView.findViewById(R.id.ImageView);
 
+        Name = findViewById(R.id.Name);
+        Username = findViewById(R.id.Username);
+        Uni = findViewById(R.id.College);
+        Confirm = findViewById(R.id.ConfirmPic);
+        profilePic = findViewById(R.id.ProfilePic);
+
         setupFirebaseListener();
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,1);
+            }
+        });
+
+        Confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserDB();
+            }
+        });
     }
 
     @Override
@@ -143,38 +188,7 @@ public class PassengerProfile extends AppCompatActivity
         return true;
     }
 
-    private void getUserDB(){
-        UserDb.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //makes sure the data is present, else the app will crash if not
-                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() >0){
-                    //data originally added is kept in this format
-                    Map<String,Object> map = (Map<String,Object>) dataSnapshot.getValue();
-                    if(map.get("Username")!=null){
-                        String DBUsername = map.get("Username").toString();
-                        NUsername.setText(DBUsername);
-                    }
-                    if(map.get("profileImageUrl")!=null){
-                        ProfilePicUrl = map.get("profileImageUrl").toString();
-                        if(ProfilePicUrl.equals("default")){
-                            //profilePic.setImageResource(R.mipmap.ic_launcher_round);
-                            navProfile.setImageResource(R.mipmap.ic_launcher_round);
-                        }
-                        else{
-                          Glide.with(getApplication()).load(ProfilePicUrl).into(navProfile);}
-                    }
 
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //not needed
-            }
-        });
-    }
 
     private void setupFirebaseListener(){
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -207,4 +221,91 @@ public class PassengerProfile extends AppCompatActivity
             FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
         }
     }
+    private void getUserDB(){
+        UserDb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //makes sure the data is present, else the app will crash if not
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() >0){
+                    //data originally added is kept in this format
+                    Map<String,Object> map = (Map<String,Object>) dataSnapshot.getValue();
+                    if(map.get("Name")!=null){
+                        DBName = map.get("Name").toString();
+                        Name.setText(DBName);
+
+                    }
+                    if(map.get("University")!=null){
+                        DBUni = map.get("University").toString();
+                        Uni.setText(DBUni);
+                    }
+                    if(map.get("Username")!=null){
+                        DBUsername = map.get("Username").toString();
+                        Username.setText(DBUsername);
+                        NUsername.setText(DBUsername);
+                    }
+                    if(map.get("profileImageUrl")!=null){
+                        ProfilePicUrl = map.get("profileImageUrl").toString();
+
+                        if(ProfilePicUrl.equals("default")){
+                            profilePic.setImageResource(R.mipmap.ic_launcher_round);
+                            navProfile.setImageResource(R.mipmap.ic_launcher_round);
+                        }
+                        else{
+                            Glide.with(getApplication()).load(ProfilePicUrl).into(profilePic);
+                            Glide.with(getApplication()).load(ProfilePicUrl).into(navProfile);
+                        }}
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //not needed
+            }
+        });
+    }
+
+    private void saveUserDB(){
+        //possibily allow the option to update details about themselves too
+        if (ResultUri != null) {
+            final StorageReference filePath = FirebaseStorage.getInstance().getReference().child("Image").child(UserID);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), ResultUri);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream Outputstream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, Outputstream);
+            byte[] data = Outputstream.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Map newImage = new HashMap();
+                            newImage.put("profileImageUrl", uri.toString());
+                            UserDb.updateChildren(newImage);
+                            finish();
+                            return;
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            finish();
+                            return;
+                        }
+                    });
+                }
+            });
+
+        };}
+
 }
